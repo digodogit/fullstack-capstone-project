32f99cf94e554250db0b079fc341b1f3c8d10f8e
Rodrigo Carvalho Quaglio Magalhaes <rodrigo.quaglio@fdte.io>
2025-06-10 13:33:07 -0300

    linting

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,13 +23,13 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require("./routes/giftRoutes")
+const giftRoutes = require("./routes/giftRoutes");
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require("./routes/searchRoutes")
+const searchRoutes = require("./routes/searchRoutes");
 
 //Auth API Task 1: import the authRoutes and store in a constant called authRoutes
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -38,14 +38,14 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('/api/gifts', giftRoutes)
+app.use('/api/gifts', giftRoutes);
 
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes)
+app.use('/api/search', searchRoutes);
 
 //Auth API Task 2: add the authRoutes to the server by using the app.use() method.
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
+    res.send("Inside the server");
 })
 
 app.listen(port, () => {
