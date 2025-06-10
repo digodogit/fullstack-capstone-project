d24d4bc882073d7456d37aeb8c031fa011ca3757
Rodrigo Carvalho Quaglio Magalhaes <rodrigo.quaglio@fdte.io>
2025-06-10 12:10:45 -0300

    module4-login

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
-
-    //insert code here to create useState hook variables for email, password
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-    // insert code here to create handleLogin function and include console.log
-  const handleLogin = () => {
-    console.log(email, password);
+  const [incorrect, setIncorrect] = useState('');
+  
+const navigate = useNavigate();
+const bearerToken = sessionStorage.getItem('bearer-token');
+const { setIsLoggedIn } = useAppContext();
+
+useEffect(() => {
+    if (sessionStorage.getItem('auth-token')) {
+      navigate('/app')
+    }
+  }, [navigate])
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    //api call
+    const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+        //Step 1 - Task 7
+        method: 'POST',
+        //Step 1 - Task 8
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+      },
+    //Step 1 - Task 9
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      })
+    });
+    const json = await res.json();
+    console.log('Json', json);
+    
+    if (json.authtoken) {
+      sessionStorage.setItem('auth-token', json.authtoken);
+      sessionStorage.setItem('name', json.userName);
+      sessionStorage.setItem('email', json.userEmail);
+
+      setIsLoggedIn(true);
+      navigate('/app');
+    } else {
+      document.getElementById("email").value="";
+      document.getElementById("password").value="";
+      setIncorrect("Wrong password. Try again.");
+      setTimeout(() => {
+        setIncorrect("");
+      }, 2000);
+    }
   }
   return (
     <div className="container mt-5">
